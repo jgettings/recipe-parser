058aafebaf4f8bb7507d33f30f8d337960f80079
Jen Gettings <jgettings@campuslabs.com>
2019-10-14 21:33:36 -0400

    dumb copied express app

---
+++ app.js
@@ -0,0 +1,20 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/api/v1/todos', (req, res) => {
+  res.status(200).send({
+    success: 'true',
+    message: 'todos retrieved successfully',
+  })
+});
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`)
+});
+
+// add eslint
+// make some tests
+// make it a real api
+// add a readme
