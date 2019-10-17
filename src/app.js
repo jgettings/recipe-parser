8c921d7690e3452211c14c98f8765143ec0f6f7e
Jen Gettings <jgettings@campuslabs.com>
2019-10-16 20:05:32 -0400

    Added eslint

--- src/app.js
+++ src/app.js
@@ -6,24 +6,24 @@ const port = process.env.PORT || 8081;
 
 
 app.get('/api/v1/parseRecipe', (req, res) => {
-  var url = req.query.url;
+  const { url } = req.query;
 
   if (!url) {
     res.status(400).send("Please include the url you want to parse as a parameter called 'url'.");
     return;
   }
 
-  var recipe = parseRecipe(url);
+  const recipe = parseRecipe(url);
 
   res.status(200).send(recipe);
 });
 
 app.listen(port, () => {
-  console.log(`server running on port ${port}`)
+  console.log(`server running on port ${port}`); // eslint-disable-line no-console
 });
 
-// add eslint
 // make some tests
 // make it a real api
 // deploy it
 // add a readme
+// swagger docs
--- src/parser.js
+++ src/parser.js
@@ -1,4 +1,4 @@
 export default (url) => ({
-    url,
-    things: 'more things'
+  url,
+  things: 'more things',
 });
